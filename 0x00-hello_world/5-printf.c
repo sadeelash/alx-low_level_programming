5dec290b67fe3dbab2a2ebca6589c60237b72891
Sadiya Yusuf <sadiatusuf@yahoo.com>
2022-09-12 21:34:57 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,11 @@
 /**
  * main - will print out grammar line
  *
- * Return: 0 Successful
+ * Return: 0 if  Successful
 **/
 
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
+}	
