47219fd9bcd984d94100ccf316bdc982cba1b7cd
Sadiya Yusuf <sadiatusuf@yahoo.com>
2022-09-12 21:13:14 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main -  will print  Programming is like building a multilingual puzzle.
+ *
+ * Return: 0 if Success
+**/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
