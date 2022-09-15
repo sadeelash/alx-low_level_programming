332af9fc2ae6d6d8eb2893d3a6925533ddca1421
Sadiya Yusuf <sadiatusuf@yahoo.com>
2022-09-15 07:58:41 -0700

    task 2

---
+++ 0x04-more_functions_nested_loops/2-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *  * main - check the code
+ *   *
+ *    * Return: Always 0.
+ *     */
+int main(void)
+{
+	    printf("%d\n", mul(98, 1024));
+	        printf("%d\n", mul(-402, 4096));
+		    return (0);
+}
+julien@ubuntu:~/0x04$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 2-main.c 2-mul.c -o 2-mul
