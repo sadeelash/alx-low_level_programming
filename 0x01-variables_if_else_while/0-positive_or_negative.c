8678e47a7d02e5f6914c3049741300bb48a4c264
sadeelash <66176747+sadeelash@users.noreply.github.com>
2022-09-16 16:18:50 +0100

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 /**
- *  * main - print whether the number stord is positive or negative. 
+ *  * main - print whether the number stord is positive or negative.
  *  * Return: Always 0 (Success)
  **/
 
@@ -17,16 +17,16 @@ int main(void)
 	
 	if (n > 0)
 	{
-		printf("%d is positive\n",n);
+		printf("%d is positive\n", n);
 	}
-	else if (n ==0)
+	else if (n == 0)
 	{
-		printf("%d is zero\n",n);
+		printf("%d is zero\n", n);
 	}
 	else
 	{
-		printf("%d is negative\n",n};
-        }
-		       
+		printf("%d is negative\n", n};
+	}
+	
 	return (0);
 }
