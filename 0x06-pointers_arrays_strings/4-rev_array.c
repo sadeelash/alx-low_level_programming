d254ad1e69d0453ef5d5a275d4a251d66ba3ef49
Sadiya Yusuf <sadiatusuf@yahoo.com>
2022-09-22 21:39:55 -0700

    task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * reverse_array - To  reverse an array
+ * @a: array of integers
+ * @n: number of elements in an array
+ *
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+	int temp;
+
+	i = 0;
+	n = n - 1;
+
+	while (i < n)
+	{
+		temp = a[i];
+		a[i] = a[n];
+		a[n] = temp;
+		i++;
+		n--;
+	}
+}
