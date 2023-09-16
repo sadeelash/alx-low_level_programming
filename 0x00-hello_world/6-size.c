cd5013eca2f0ff881fc5672027cd8ab6914e0853
Sadiya <sadiatusuf@yahoo.com>
2023-09-16 10:39:14 -0700

    tasks

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints the size of various types on th ecomputer it is compiled and run on.
+ * main - prints the size of various types on the computer it is compiled.
  * Return: 0
 **/
 
