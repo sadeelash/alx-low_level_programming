71bf19e5392c2a1e99c6818334e66341403b00d1
Sadiya <sadiatusuf@yahoo.com>
2023-09-18 05:59:21 -0700

    task

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the first 98 Fibonacci numbers separated by a comma
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	unsigned long a = 1, b = 2, c;
+	int i;
+
+	printf("%lu, %lu", a, b);
+
+	for (i = 3; i <= 98; i++)
+	{
+		c = a + b;
+		printf(", %lu", c);
+		a = b;
+		b = c;
+	}
+	printf("\n");
+
+	return (0);
+}
+
