c9ce3f9252e8015fa0fa57e3f396f31e311103f0
Sadiya <sadiatusuf@yahoo.com>
2023-09-18 07:58:17 -0700

    tasko

---
+++ 0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* main - tests that function positive or negative gives correct output
+* Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
