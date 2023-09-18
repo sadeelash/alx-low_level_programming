b171367796aeb46128c978a819c126fb08e67de4
Sadiya <sadiatusuf@yahoo.com>
2023-09-18 05:01:25 -0700

    tasks

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,27 +5,27 @@
  */
 void times_table(void)
 {
-    int i, j, result;
+	int i, j, result;
 
-    for (i = 0; i <= 9; i++)
-    {
-        _putchar('0');
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar('0');
 
-        for (j = 1; j <= 9; j++)
-        {
-            result = i * j;
-            _putchar(',');
-            _putchar(' ');
+		for (j = 1; j <= 9; j++)
+		{
+			result = i * j;
+			_putchar(',');
+			_putchar(' ');
 
-            if (result < 10)
-                _putchar(' ');
-            
-            else
-                _putchar((result / 10) + '0');
+			if (result < 10)
+				_putchar(' ');
 
-            _putchar((result % 10) + '0');
-        }
-        _putchar('\n');
-    }
+			else
+				_putchar((result / 10) + '0');
+
+			_putchar((result % 10) + '0');
+		}
+		_putchar('\n');
+	}
 }
 
