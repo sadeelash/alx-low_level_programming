ee4c1e1a4318794f7088429b147a42b7ad51328a
Sadiya <sadiatusuf@yahoo.com>
2023-09-21 18:42:14 -0700

    task 12

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,23 +1,54 @@
+#include "main.h"
 #include <stdio.h>
 
-void print_buffer(char *b, int size);
-
-int main(void)
+/**
+ * print_buffer - Prints a buffer
+ * @b: The buffer to print
+ * @size: The size of the buffer
+ *
+ * Return: void
+ */
+void print_buffer(char *b, int size)
 {
-    char buffer[98] = "0 1 2 3 4 5 6 7 8 9 a b c d e f \
-                     0123456789abcdef\
-                     000000000011111111112222222222333333333344444444445555555555\
-                     0123456789012345678901234567890123456789012345678901234567890123456789";
-    char buffer2[98] = "0a1b2c3d4e5f\
-                     0123456789abcdef\
-                     000000000011111111112222222222333333333344444444445555555555\
-                     0123456789012345678901234567890123456789012345678901234567890123456789";
+    int offset, j, i;
 
-    printf("Buffer 1:\n");
-    print_buffer(buffer, 98);
-    printf("\nBuffer 2:\n");
-    print_buffer(buffer2, 98);
+    offset = 0;
 
-    return (0);
+    if (size <= 0)
+    {
+        printf("\n");
+        return;
+    }
+
+    while (offset < size)
+    {
+        j = size - offset < 10 ? size - offset : 10;
+        printf("%08x: ", offset);
+
+        for (i = 0; i < 10; i++)
+        {
+            if (i < j)
+                printf("%02x", *(b + offset + i));
+            else
+                printf("  ");
+            if (i % 2)
+            {
+                printf(" ");
+            }
+        }
+
+        for (i = 0; i < j; i++)
+        {
+            int c = *(b + offset + i);
+
+            if (c < 32 || c > 126)
+            {
+                c = '.';
+            }
+            printf("%c", c);
+        }
+        printf("\n");
+        offset += 10;
+    }
 }
 
