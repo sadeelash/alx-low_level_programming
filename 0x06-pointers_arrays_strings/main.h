ff80c8feaf248b66e9537e0fa71eea554d6aca34
Sadiya <sadiatusuf@yahoo.com>
2023-09-21 18:43:19 -0700

    task 12

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -13,4 +14,6 @@ void print_number(int n);
 int _putchar(char c);
 void print_buffer(char *b, int size);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
+
+
 #endif /*MAIN_H*/
