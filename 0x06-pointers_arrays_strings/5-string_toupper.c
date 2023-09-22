4a259aa4b44e8cac85df7535344aafa017510f3b
Sadiya <sadiatusuf@yahoo.com>
2023-09-21 17:03:57 -0700

    task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+/**
+ * string_toupper - Changes all lowercase letters of a string to uppercase
+ * @str: The string to be modified
+ *
+ * Return: A pointer to the modified string
+ */
+char *string_toupper(char *str)
+{
+	char *ptr = str;
+
+	while (*ptr)
+	{
+		if (*ptr >= 'a' && *ptr <= 'z')
+		{
+			*ptr = *ptr - ('a' - 'A');
+		}
+		ptr++;
+	}
+
+	return str;
+}
+
