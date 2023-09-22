8e48f668bb7cce3f821aef63fc319600c1318830
Sadiya <sadiatusuf@yahoo.com>
2023-09-21 18:27:20 -0700

    task 10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -9,16 +9,15 @@ int main(void)
   a[2] = 1024;
   p = &n;
   /*
-   * Write your line of code here...
+   * write your line of code here...
    * Remember:
-   * - You are not allowed to use variable a.
-   * - You are not allowed to modify p.
-   * - Only one statement is allowed.
-   * - You are not allowed to code anything else than this line of code.
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
    */
-  *p = 98; // Set the value pointed to by p to 98
+  *(p + 5) = 98;
   /* ...so that this prints 98\n */
   printf("a[2] = %d\n", a[2]);
   return (0);
 }
-
