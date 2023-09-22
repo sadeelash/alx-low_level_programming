59620b7b855e9050aa21508db639f1eed7c7d2ed
Sadiya <sadiatusuf@yahoo.com>
2023-09-21 17:25:33 -0700

    task 9

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+/**
+ * rot13 - Encodes a string using rot13.
+ * @str: The string to be encoded.
+ *
+ * Return: A pointer to the encoded string.
+ */
+char *rot13(char *str)
+{
+	char *s = str;
+	char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; alpha[i]; i++)
+		{
+			if (*s == alpha[i])
+			{
+				*s = rot13[i];
+				break;
+			}
+		}
+		s++;
+	}
+	return (str);
+}
+
