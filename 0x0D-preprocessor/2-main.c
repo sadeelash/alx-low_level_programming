83476fc352c37f43340ee8edf9bb73916a6ef3ff
Sadiya <sadiatusuf@yahoo.com>
2023-10-10 21:26:45 -0700

    task 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: This program prints the name of the source file.
+ * It uses the predefined macro __FILE__ to obtain the file name.
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	printf("%s\n", __FILE__);
