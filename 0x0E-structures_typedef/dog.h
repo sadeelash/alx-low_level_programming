c338c9500867760280d80ff1d3ead1b5b37cef43
Sadiya <sadiatusuf@yahoo.com>
2023-10-10 19:35:53 -0700

    task 1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,10 +7,11 @@
  * @age: The age of the dog (a float).
  * @owner: The owner of the dog (a string).
  */
-struct dog {
-    char *name;
-    float age;
-    char *owner;
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
 };
 
 #endif /* DOG_H */
