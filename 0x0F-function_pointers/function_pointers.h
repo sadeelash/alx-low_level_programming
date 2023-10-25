5f0374d53ca4106406d49fd3f96bbb8d4aa295d7
Sadiya <sadiatusuf@yahoo.com>
2023-10-25 15:09:50 -0700

    task 0

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,15 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+/**
+ * File: function_pointers.h
+ *
+ * Desc: Header file for function pointers.
+ **/
+
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
+#endif
+
