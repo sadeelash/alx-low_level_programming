5873b076bd6962826654654d41f5fcac13402514
Sadiya <sadiatusuf@yahoo.com>
2023-10-25 15:23:28 -0700

    task 2

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,16 +1,19 @@
+#include <stdio.h>
 #include "function_pointers.h"
 
 /**
- * array_iterator - Execute a function on each element of an array
- * @array: The array to process
+ * array_iterator - Executes a function given as a parameter on each element of an array
+ * @array: The array to iterate through
  * @size: The size of the array
- * @action: A pointer to the function to execute
+ * @action: A pointer to the function to be used on each element
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
     if (array != NULL && action != NULL)
     {
-        for (size_t i = 0; i < size; i++)
+        size_t i;
+
+        for (i = 0; i < size; i++)
         {
             action(array[i]);
         }
---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * int_index - Searches for an integer in an array.
+ * @array: The array to search in.
+ * @size: The number of elements in the array.
+ * @cmp: A pointer to a function to compare values.
+ *
+ * Return: Index of the first element for which cmp doesn't return 0.
+ *         If no element matches, return -1.
+ *         If size <= 0, return -1.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	if (array == NULL || cmp == NULL || size <= 0)
+		return (-1);
+
+	for (int i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+
+	return (-1);
+}
+
